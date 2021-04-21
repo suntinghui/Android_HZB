66faf5bdd551c9937b31aee7079e56f34223e40f
tinghuisun@163.com <tinghuisun@163.com>
2021-04-21 21:11:06 +0800

    STH

---
+++ app/src/main/java/com/cfastech/yonghu/MyApplication.java
@@ -0,0 +1,11 @@
+package com.cfastech.yonghu;
+
+import io.dcloud.application.DCloudApplication;
+
+public class MyApplication extends DCloudApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
